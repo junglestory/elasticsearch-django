e112f709f0b21b6d223d95bbc0b6140a3c318028
김덕규 <monocaine@gmail.com>
2018-03-26 23:16:01 +0900

    Add search templates

--- search/views.py
+++ search/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.http import HttpResponse
 
 def search(request):
-    return HttpResponse('Hello!!')
+    msg = 'My Message'
+    return render(request, 'search.html', {'message': msg})
